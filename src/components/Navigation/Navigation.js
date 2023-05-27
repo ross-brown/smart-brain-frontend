627fab4c3e8e5ac38b57da0cdb23aaadda188d4c
ross-brown <rbrown_98@yahoo.com>
2023-05-27 14:52:41 -0500

    create ImageLinkForm component

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Navigation = (props) => {
+const Navigation = () => {
   return (
     <nav style={{display: "flex", justifyContent: "flex-end"}}>
       <p className="f3 link dim black underline pa3 pointer">Sign Out</p>
